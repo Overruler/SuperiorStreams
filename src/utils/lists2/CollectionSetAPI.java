b92bafb45a162b8f0f5b62d23c51f07251c2488a
Timo Kinnunen <timo.kinnunen@gmail.com>
2014-09-20 23:04:31 +0300

    Refactor Collections to use interfaces, intermediate step 3.
    Signed-off-by: Timo Kinnunen <timo.kinnunen@gmail.com>

--- src/utils/lists2/CollectionSetAPI.java
+++
@@ -1,9 +0,0 @@
-package utils.lists2;
-
-import utils.streams.functions.ExFunction;
-
-interface CollectionSetAPI<T, SET extends CollectionSetAPI<T, SET>> extends RandomLookup<T, SET> {
-	<U, E extends Exception> Object map(ExFunction<T, U, E> mapper) throws E;
-	List<T> toList();
-	ArrayList<T> toArrayList();
-}
